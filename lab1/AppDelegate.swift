42e5a27f98daf66a45f14e72220d2ff21ee2f5ba
Roman <zummo3@gmail.com>
2016-10-02 12:13:06 +0300

    Initial Commit

---
+++ lab1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lab1
+//
+//  Created by Roman Faizullin on 02/10/2016.
+//  Copyright © 2016 Roman Faizullin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ lab1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lab1
+//
+//  Created by Roman Faizullin on 02/10/2016.
+//  Copyright © 2016 Roman Faizullin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
